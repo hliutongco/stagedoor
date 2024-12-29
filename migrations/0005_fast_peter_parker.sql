22b9cd38be23ee4ad70b96c3850d6b798a92ce33
Helen Liutongco <liutongcoh@gmail.com>
2024-12-29 14:40:33 -0500

    fix(migrations): drop id columns and add new uuid columns

--- migrations/0005_fast_peter_parker.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "reviews" DROP COLUMN IF EXISTS "id";--> statement-breakpoint
-ALTER TABLE "shows" DROP COLUMN IF EXISTS "id";
---
+++ migrations/0005_little_loners.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "reviews" DROP COLUMN IF EXISTS "id";--> statement-breakpoint
+ALTER TABLE "reviews" DROP COLUMN IF EXISTS "show_id";--> statement-breakpoint
+ALTER TABLE "reviews" DROP COLUMN IF EXISTS "user_id";--> statement-breakpoint
+ALTER TABLE "shows" DROP COLUMN IF EXISTS "id";
