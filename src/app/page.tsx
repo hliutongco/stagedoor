1fea55441f1858d3c750f24cdf4f84fedbccf119
Helen Liutongco <liutongcoh@gmail.com>
2024-12-29 12:37:24 -0500

    chore(app/page): add todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,11 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ShowsList />
       </main>
-      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer> */}
     </div>
   );
+}
+
+// TODO: eventually add in a footer
+{
+  /* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer> */
 }
