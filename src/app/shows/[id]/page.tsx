2d9c13c63b05cebe305d0e1a775f07159fd2accd
Helen Liutongco <liutongcoh@gmail.com>
2025-01-05 18:59:01 -0500

    fix(shows/page): add star rating component base

--- src/app/shows/[id]/page.tsx
+++ src/app/shows/[id]/page.tsx
@@ -2,6 +2,7 @@ import { trpc } from '@/server/clients/server-api';
 import WatchedButton from '../components/WatchedButton';
 import { currentUser } from '@clerk/nextjs/server';
 import WatchedCount from '../components/WatchedCount';
+import StarRating from '@/components/star-rating';
 
 export default async function ShowView({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
@@ -14,6 +15,7 @@ export default async function ShowView({ params }: { params: Promise<{ id: strin
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {show.title}
+      <StarRating value={3.5} />
       <WatchedCount showId={id} />
       <WatchedButton id={id} isWatched={Boolean(watchedShow)} userId={user?.id} />
     </div>
