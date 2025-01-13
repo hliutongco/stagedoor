3e33d736e87d56817a170f00272804b20586a0b2
Helen Liutongco <liutongcoh@gmail.com>
2025-01-12 20:18:18 -0500

    fix(ShowsList): begin differentiating btwn plays and musicals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ export const metadata: Metadata = {
 };
 
 export default async function Home() {
-  const shows = await trpc.shows.getShows();
+  const musicals = await trpc.shows.getMusicals();
+  const plays = await trpc.shows.getPlays();
 
   return (
     <main>
@@ -18,12 +19,14 @@ export default async function Home() {
         <div className="mt-5">
           <h3 className="font-medium text-3xl">Musicals</h3>
           <Separator className="mb-3 bg-secondary" />
-          <div className="mx-auto w-3/4">{shows && <ShowsList shows={shows} />}</div>
+          <div className="mx-auto w-3/4">
+            {musicals && <ShowsList shows={musicals} />}
+          </div>
         </div>
         <div className="mt-5">
           <h3 className="font-medium text-3xl">Straight Plays</h3>
           <Separator className="mb-3 bg-secondary" />
-          <div className="mx-auto w-3/4">{shows && <ShowsList shows={shows} />}</div>
+          <div className="mx-auto w-3/4">{plays && <ShowsList shows={plays} />}</div>
         </div>
       </div>
       <div className="bg-primary min-h-screen sm:p-10"></div>
