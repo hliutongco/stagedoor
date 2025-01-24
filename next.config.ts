bc79a5ec6d5ae5336f200c123117fce7f313ba41
Helen Liutongco <liutongcoh@gmail.com>
2025-01-24 08:10:04 +0800

    fix(next.config.ts): refactor to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [process.env.AWS_URL!, 'img.clerk.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: process.env.AWS_URL!,
+        pathname: '**',
+      },
+    ],
   },
 };
 
