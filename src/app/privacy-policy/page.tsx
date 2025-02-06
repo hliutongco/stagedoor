bc35b09ce534f5680c63a1321c5a82d682d2bcb7
Helen L. <32890150+hliutongco@users.noreply.github.com>
2025-02-06 08:14:59 -0500

    fix(privacy-policy): add a privacy policy page (#67)

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="h-[100vh] mt-10">
+      <iframe height="100%" src="/privacy-policy.html" width="100%"></iframe>;
+    </div>
+  );
+}
