94e4623ed67a8b471619ccf4a1d5ce761a7d2b0d
Helen Liutongco <liutongcoh@gmail.com>
2025-03-13 17:46:18 -0400

    fix(ReviewCollection): edit homepage to also use ReviewCollection

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,12 +2,7 @@ import { Metadata } from 'next';
 import ShowsList from '../components/ShowsList';
 import { trpc } from '@/server/clients/server-api';
 import { Separator } from '@/components/ui/';
-import StarRating from '@/components/core/star-rating';
-import Link from 'next/link';
-import CloudinaryImage from '@/app/components/CloudinaryImage';
-import Image from 'next/image';
-import { transformCharacters } from '@/lib/utils/index';
-import ReviewBody from '../components/ReviewBody';
+import ReviewCollection from '../my-profile/components/ReviewCollection';
 
 export const metadata: Metadata = {
   title: 'StageDoor',
@@ -38,55 +33,7 @@ export default async function Home() {
           <div className="mx-auto w-3/4">{plays && <ShowsList shows={plays} />}</div>
         </div>
       </div>
-      <div className="bg-primary-light min-h-[95vh] p-10">
-        <h3 className="font-medium text-xl lg:text-3xl">Reviews</h3>
-        <div className="gap-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 items-center mt-8">
-          {reviews.map((review) => (
-            <div
-              className="bg-background flex flex-col h-full mb-10 p-4 relative rounded-md w-full"
-              key={review.id}
-            >
-              <Link href={`/reviews/${review.id}`}>
-                <h4 className="font-bold text-center text-lg lg:text-xl underline">
-                  {review.title}
-                </h4>
-              </Link>
-              {Boolean(review.userShow?.rating) && review.userShow?.rating !== '0' && (
-                <div className="mx-auto">
-                  <StarRating name={review.id} value={`${review.userShow?.rating}`} />
-                </div>
-              )}
-              <div className="mt-2">
-                <Link href={`/shows/${review.show?.slug}`}>
-                  <CloudinaryImage
-                    alt={review.show?.title ?? ''}
-                    className="float-left mx-2"
-                    height={150}
-                    src={transformCharacters(review.show?.slug ?? '')}
-                    style={{ height: '150px', width: '100px' }}
-                    width={100}
-                  />
-                </Link>
-                <ReviewBody body={review.body} id={review.id} length={300} />
-              </div>
-              <Link href={`/users/${review.user?.username}`}>
-                <div className="absolute bottom-3 flex gap-2 items-center">
-                  <Image
-                    alt={review.user?.username + 'profile picture'}
-                    aria-hidden
-                    className="rounded-2xl"
-                    height={30}
-                    src={review.user?.imageUrl ?? ''}
-                    style={{ height: 'auto', width: 'auto' }}
-                    width={30}
-                  />
-                  {review.user?.username}
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div>
+      <ReviewCollection displayUser reviews={reviews} />
     </main>
   );
 }
