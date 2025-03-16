5442b3359ef264ad4f8c5650394de22a89faed8b
Helen Liutongco <liutongcoh@gmail.com>
2025-03-15 23:49:43 -0400

    fix(reviews/loading): tweak loading screens

--- src/app/my-profile/reviews/loading.tsx
+++ src/app/my-profile/reviews/loading.tsx
@@ -2,36 +2,22 @@ import { Skeleton } from '@/components/ui/';
 
 export default function Loading() {
   return (
-    <>
-      <div className="flex flex-col items-center min-h-[95vh] mt-6 p-4 lg:p-8 pb-20">
-        <Skeleton className="bg-secondary h-[50px] lg:h-[65px] rounded-md w-1/3" />
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
-          <div>
-            <Skeleton className="bg-secondary h-[235px] md:h-[400px] lg:h-[260px] xl:h-[368px] mt-2 rounded-md w-[150px] md:w-[230px] lg:w-[160px] xl:w-[230px]" />
-            <Skeleton className="bg-secondary h-[25px] mt-2 rounded-md w-[150px]" />
-          </div>
-          <div>
-            <Skeleton className="bg-secondary h-[235px] md:h-[400px] lg:h-[260px] xl:h-[368px] mt-2 rounded-md w-[150px] md:w-[230px] lg:w-[160px] xl:w-[230px]" />
-            <Skeleton className="bg-secondary h-[25px] mt-2 rounded-md w-[150px]" />
-          </div>
-          <div>
-            <Skeleton className="bg-secondary h-[235px] md:h-[400px] lg:h-[260px] xl:h-[368px] mt-2 rounded-md w-[150px] md:w-[230px] lg:w-[160px] xl:w-[230px]" />
-            <Skeleton className="bg-secondary h-[25px] mt-2 rounded-md w-[150px]" />
-          </div>
-          <div>
-            <Skeleton className="bg-secondary h-[235px] md:h-[400px] lg:h-[260px] xl:h-[368px] mt-2 rounded-md w-[150px] md:w-[230px] lg:w-[160px] xl:w-[230px]" />
-            <Skeleton className="bg-secondary h-[25px] mt-2 rounded-md w-[150px]" />
-          </div>
-          <div>
-            <Skeleton className="bg-secondary h-[235px] md:h-[400px] lg:h-[260px] xl:h-[368px] mt-2 rounded-md w-[150px] md:w-[230px] lg:w-[160px] xl:w-[230px]" />
-            <Skeleton className="bg-secondary h-[25px] mt-2 rounded-md w-[150px]" />
-          </div>
-          <div>
-            <Skeleton className="bg-secondary h-[235px] md:h-[400px] lg:h-[260px] xl:h-[368px] mt-2 rounded-md w-[150px] md:w-[230px] lg:w-[160px] xl:w-[230px]" />
-            <Skeleton className="bg-secondary h-[25px] mt-2 rounded-md w-[150px]" />
-          </div>
+    <div className="flex flex-col min-h-[95vh] mt-6 p-4 lg:p-8 pb-20">
+      <Skeleton className="bg-secondary h-[50px] lg:h-[65px] rounded-md w-1/3" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 items-center mt-4">
+        <div>
+          <Skeleton className="bg-secondary h-[325px] md:h-[350px] lg:h-[400px] xl:h-[500px] mt-2 rounded-md w-[290px] md:w-[300px] lg:w-[310px] xl:w-[340px]" />
+        </div>
+        <div>
+          <Skeleton className="bg-secondary h-[325px] md:h-[350px] lg:h-[400px] xl:h-[500px] mt-2 rounded-md w-[290px] md:w-[300px] lg:w-[310px] xl:w-[340px]" />
+        </div>
+        <div>
+          <Skeleton className="bg-secondary h-[325px] md:h-[350px] lg:h-[400px] xl:h-[500px] mt-2 rounded-md w-[290px] md:w-[300px] lg:w-[310px] xl:w-[340px]" />
+        </div>
+        <div>
+          <Skeleton className="bg-secondary h-[325px] md:h-[350px] lg:h-[400px] xl:h-[500px] mt-2 rounded-md w-[290px] md:w-[300px] lg:w-[310px] xl:w-[340px]" />
         </div>
       </div>
-    </>
+    </div>
   );
 }
